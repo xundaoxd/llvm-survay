bc96adf2bbf70487b4a997dbb07500c9b437b67a
xundaoxd <xundaoxd@outlook.com>
2023-05-21 21:36:31 +0800

    basic macro-expand: using PrintPreprocessedAction

--- src/macro-expand/MacroExpand.cpp
+++ src/macro-expand/MacroExpand.cpp
@@ -1,6 +1,22 @@
-#include <iostream>
+#include "clang/Frontend/FrontendActions.h"
+#include "clang/Frontend/FrontendOptions.h"
+#include "clang/Tooling/CommonOptionsParser.h"
+#include "clang/Tooling/Tooling.h"
+#include "llvm/Support/CommandLine.h"
 
-int main(int argc, char *argv[]) {
-  std::cout << "Hello World" << std::endl;
-  return 0;
+static llvm::cl::OptionCategory MacroExpandCategory("macro-expand options");
+
+int main(int argc, const char **argv) {
+  llvm::Expected<clang::tooling::CommonOptionsParser> OptionsParser =
+      clang::tooling::CommonOptionsParser::create(argc, argv,
+                                                  MacroExpandCategory);
+  if (!OptionsParser) {
+    llvm::errs() << OptionsParser.takeError() << '\n';
+    return 1;
+  }
+  clang::tooling::ClangTool Tool(OptionsParser->getCompilations(),
+                                 OptionsParser->getSourcePathList());
+  return Tool.run(
+      clang::tooling::newFrontendActionFactory<clang::PrintPreprocessedAction>()
+          .get());
 }
